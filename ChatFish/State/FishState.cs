505fc3cf72322dd2a3818be4f2251bc3587f13ac
Don Kackman <dkackman@gmail.com>
2024-10-07 09:38:45 -0500

    wired in message changed event

--- ChatFish/State/FishState.cs
+++ ChatFish/State/FishState.cs
@@ -2,12 +2,57 @@ using ChatFish.Components;
 
 namespace ChatFish.State;
 
-public class FishState
+public class FishState : IDisposable
 {
+    private ChatMessage _currentMessage = new();
+    private bool disposedValue;
+    private CancellationTokenSource _messageCts = new();
+
     public string Id { get; init; } = string.Empty;
     public FishColor Color { get; init; } = FishColor.Orange;
     public string Scale { get; init; } = "100.0";
-    public ChatMessage CurrentMessage { get; set; } = new();
-    public bool IsMessageVisible { get; set; } = false;
+    public bool IsMessageVisible { get; private set; } = false;
+
+    public ChatMessage CurrentMessage
+    {
+        get => _currentMessage;
+        set
+        {
+            if (value != _currentMessage)
+            {
+                _currentMessage = value;
+                OnMessageChanged();
+            }
+        }
+    }
+
+    private void OnMessageChanged()
+    {
+        IsMessageVisible = !_currentMessage.IsEmpty;
+        MessageChanged?.Invoke();
+    }
+
+    public Action? MessageChanged;
+
     override public string ToString() => $"[Id: {Id}, Color: {Color}]";
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                _messageCts.Dispose();
+            }
+
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
 }
